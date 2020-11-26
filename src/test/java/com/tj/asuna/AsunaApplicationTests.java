a858698314943284b40498d1444575897c386277
倪天熠 <nitainyi@shouqianba.com>
2020-11-26 22:46:48 +0800

    init

---
+++ src/test/java/com/tj/asuna/AsunaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tj.asuna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsunaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
